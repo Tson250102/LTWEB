d8ce5cd75331adc26a9a28a72133ab745ee49381
Nien1409 <nienkook@gmail.com>
2022-06-20 22:22:57 +0800

    '13'

--- Nam/btlon.js
+++ Nam/btlon.js
@@ -1,20 +1,9 @@
-// $(document).ready(function() {
-// $('#myId').on('keyup', function(event){
-// event.preventDefault();
-// // vong lap
-// var tukhoa = $(this).val().toLowerCase();
-// $('.card-body h5').filter(function(){
-// $(this).toggle($(this).text().toLowerCase().indexOf(tukhoa)>-1);
-// });
-// });
-// });
-alert("abc");
 function myFunction(){
     var input, filter , tb , tr , td , i;
     input = document.getElementById("tk");
     filter = input.value.toUpperCase();
     tb = document.getElementById("div_right");
-    tr = tb.getElementsByTagName("div");
+    tr = tb.getElementsByClassName("ten");
     for(i=0;i<tr.length;i++){
         td = tr[i].getElementsByTagName("h5")[0];
         if(td){
@@ -25,5 +14,4 @@ function myFunction(){
             }
         }
     }
-
 }
